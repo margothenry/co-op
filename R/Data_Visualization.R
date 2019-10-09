6e629ca1158d7da1f00f4708fd1a5985560e78c4
tql99 <let34568@myumanitoba.ca>
2019-10-09 15:47:31 -0500

    In data_out/images: Added a graph of c-hyper vs generation, endpoints only, color-coded by species.

--- R/Data_Visualization.R
+++ R/Data_Visualization.R
@@ -4,6 +4,7 @@ library(Hmisc)
 library(RColorBrewer)
 library(janitor)
 library(here)
+library (scales)
 ##############################
 ### (TL): Below are the codes I use to generate the visualizations in "data_out/images".
 ##############################
@@ -28,9 +29,11 @@ aeruginosa_entries_index <- grep("aeruginosa", master_analyses$species)
 aeruginosa_entries <- master_analyses[aeruginosa_entries_index,]
 (aeruginosa_paper_count <- length(unique(aeruginosa_entries$paper)))
 
+# Replace N/A values for "strain_info" with "0".
 master_analyses$strain_info <- master_analyses$strain_info %>%
   replace(is.na(.), "0")
-### If there are multiple strain backgrounds in 1 paper, include the value for "strain_info" in the "paper" column (for now).
+
+# If there are multiple strain backgrounds in 1 paper, include the value for "strain_info" in the "paper" column (for now).
 for (i in 1:nrow(master_analyses)) {
   if (master_analyses[i, "strain_info"] != "0"){
     master_analyses[i, "paper"] <- paste(master_analyses[i, "paper"], " ", "(", master_analyses[i, "strain_info"], ")", sep = "")
@@ -117,17 +120,23 @@ ggplot(multiple_wide_generation_analysis, aes(x = generation, y = c_hyper, color
 cor(multiple_wide_generation_analysis$c_hyper, multiple_wide_generation_analysis$generation)
 
 # c-hyper vs generation (generation-notated end points only):
+end_point_generation_analysis <- c()
 end_point_generation_analysis <- generation_analysis %>%
   subset(func != "multiple_wide")
-### Get only the end points of experiments with data for multiple generations:
-### TL deliberately omitted the endpoint of Tenaillon2016 - way beyond the range of the other experiments, shifting the regression line too much.
+### Get only the end points of experiments with data for multiple generations (from multiple_wide_generation_analysis):
 ### [The selection of rows in this step is still manual & can be made automatic].
-end_point_entries_multiple_wide <- c(6, 8, 12, 17, 28, 36, 46, 59)
+end_point_entries_multiple_wide <- c(6, 8, 12, 17, 28, 36, 46, 57, 59)
 end_point_generation_analysis <- rbind(end_point_generation_analysis, multiple_wide_generation_analysis[end_point_entries_multiple_wide,])
-ggplot(end_point_generation_analysis, aes(x = generation, y = c_hyper)) + geom_point() +  geom_smooth(method=lm, se=FALSE) + labs(color = "Datasets") + 
-  ggtitle("c-hyper vs generation, generation-notating end points only") + theme(plot.title = element_text(hjust = 0.5, size = 11), legend.position = "bottom") 
+### The plot is currently optimized for the poster competition (bigger axis.text, bigger legend.title, etc.)
+ggplot(end_point_generation_analysis, aes(x = generation, y = c_hyper, color = species)) + xlab("Generations") + ylab("c-hyper") + 
+  geom_jitter(size = 3, width = 0.12) + labs(color = "Species") + scale_x_log10() +
+  theme(axis.text = element_text(size = 14), axis.title = element_text(size = 18, face = "bold"), 
+        legend.title = element_text(size = 18, face = "bold"), legend.position = "right", legend.text = element_text(size = 14, face = "italic")) 
+# Include this if need a title: "+ ggtitle("c-hyper vs generation, generation-notating end points only") + theme(plot.title = element_text(hjust = 0.5, size = 11))" 
+# Include this if need a linear regression line: "+ geom_smooth(method=lm, se = FALSE)"
+# Include this if need a log curve: "+ geom_smooth(method = lm, formula = y ~ log(x), se = FALSE)"
 
-## End-point analysis, by species:
+## c-hyper vs generation (generation-notated end points only), by species:
 ### E. coli (also omitting Tenaillon2016 for the same reason as mentioned above):
 end_point_generation_Ecoli_entries <- grep("E.", end_point_generation_analysis$species)
 end_point_generation_Ecoli_analysis <- end_point_generation_analysis[end_point_generation_Ecoli_entries,]
